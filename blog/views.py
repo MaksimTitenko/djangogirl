19c29cd31c0aea4aa0ccbdfcf9a62f454947160f
maks <domen699@gmail.com>
2018-08-19 00:54:00 +0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def index(request):
-    return HttpResponse('<h1>Питончик<h1>')
+def post_list(request):
+    return render(
+        request,
+        'blog/post_list.html',
+        {}
+    )
